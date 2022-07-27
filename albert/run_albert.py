bbe165662c62bdc7c97916e0731b0e24c2d1191d
aislinblack <1of4black@gmail.com>
2022-07-27 18:41:55 -0400

    Followed a new tutorial for albert https://colab.research.google.com/github/spark-ming/albert-qa-demo/blob/master/Question_Answering_with_ALBERT.ipynb

--- albert/run_albert.py
+++ albert/run_albert.py
@@ -1,10 +1,13 @@
-from albert_v2 import albert
+
+from albert import run_prediction
 
 context = "New Zealand (Māori: Aotearoa) is a sovereign island country in the southwestern Pacific Ocean. It has a total land area of 268,000 square kilometres (103,500 sq mi), and a population of 4.9 million. New Zealand's capital city is Wellington, and its most populous city is Auckland."
 questions = ["How many people live in New Zealand?",
              "What's the largest city?"]
 
 # Run method
-predictions = albert({'question': 'What organization is the IPCC a part of?', 'context': context})
+predictions = run_prediction(questions, context)
 
-print("prediction")
+# Print results
+for key in predictions.keys():
+  print(predictions[key])
